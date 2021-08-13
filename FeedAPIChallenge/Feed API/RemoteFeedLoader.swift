596fbcb8b2a52edd1dbeeaa1055ecf418d5811f5
Lorenzo Angelini <lorenzoangelini@MacBook-Pro.local>
2021-08-13 16:15:19 +0200

    feat(test-fourth-case): resolve fourth test case, mapping the response e detect if data is valid

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,13 +21,43 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case let .success((_, response)):
-				if response.statusCode != 200 {
-					completion(.failure(Error.invalidData))
-				}
+			case let .success((data, response)):
+				completion(FeedLoaderMapper.map(data, from: response))
+
 			case .failure(_):
 				completion(.failure(Error.connectivity))
 			}
 		}
+	}
+}
+
+final class FeedLoaderMapper {
+	private struct Root: Codable {
+		let items: [Image]
+		var images: [FeedImage] {
+			return items.map {
+				$0.item
+			}
+		}
+	}
+
+	private struct Image: Codable {
+		let image_id: UUID
+		let image_desc: String?
+		let image_loc: String?
+		let image_url: URL
+
+		var item: FeedImage {
+			FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
+		}
+	}
+
+	private static let STATUS_200 = 200
+
+	static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+		guard response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+			return .failure(RemoteFeedLoader.Error.invalidData)
+		}
+		return .success(root.images)
 	}
 }
